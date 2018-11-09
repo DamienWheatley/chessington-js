fbdac7f539b4da5d592f2fa332c18f1d3622236d
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2018-11-09 14:49:14 +0000

    Cherry Pick 4 complete, Bishops can move diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,11 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        if(this.player === Player.WHITE){
+            let square = [];
+            square.push(new Square(0,1),new Square(1,2),new Square(3,4),new Square(4,5),new Square(5,6),new Square(6,7),new Square(0,5)
+            ,new Square(1,4),new Square(3,2),new Square(4,1),new Square(5,0));
+            return square;
+        }
     }
 }
